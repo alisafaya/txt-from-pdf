0186669df300708a363dfdeb29d55100581c016c
Ali Safaya <alisafaya@gmail.com>
2024-07-25 17:08:24 +0300

    Release: v1.2.3

--- txtfrompdf/__init__.py
+++ txtfrompdf/__init__.py
@@ -2,4 +2,4 @@ from .extract import extract_txt_from_pdf, get_size_per_page, split_pdf, pdf_to_
 from .filter import post_process # noqa
 
 __all__ = ["extract_txt_from_pdf", "get_size_per_page", "split_pdf", "pdf_to_text", "post_process"]
-__version__ = "1.2.2"
+__version__ = "1.2.3"
